c5cc514b95d1840219e9af985808e65c338776b0
tokukiyu <tokkiyuam@gmail.com>
2023-12-29 22:28:56 +0300

    while loop, for in loop

--- loop.dart
+++ loop.dart
@@ -3,5 +3,14 @@ void main() {
   for (int a = 0; a <= num; a++) {
     // print('$a: hello');
   }
+// for in loop
+  var names = ['ne', 'no', 'ni', 'bi'];
+  for (var a in names) {
+    // print(a);
+  }
 
+  while (num >= 1) {
+    print(num);
+    num--;
+  }
 }
